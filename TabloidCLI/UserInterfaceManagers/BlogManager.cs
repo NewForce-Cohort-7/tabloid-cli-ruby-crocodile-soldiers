486caf7714716b58b27770471cf7f4452066e23b
JamesB0907 <virverum22@gmail.com>
2023-06-16 11:53:03 -0400

    Implemented and tested INSERT FEATURE for blogs

---
+++ TabloidCLI/UserInterfaceManagers/BlogManager.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using TabloidCLI.Models;
+
+namespace TabloidCLI.UserInterfaceManagers
+{
+    public class BlogManager : IUserInterfaceManager
+    {
+        private readonly IUserInterfaceManager _parentUI;
+        private BlogRepository _blogRepository;
+        private string _connectionString;
+
+        public BlogManager(IUserInterfaceManager parentUI, string connectionString)
+        {
+            _parentUI = parentUI;
+            _blogRepository = new BlogRepository(connectionString);
+            _connectionString = connectionString;
+        }
+
+        public IUserInterfaceManager Execute()
+        {
+            Console.WriteLine("Blog Menu");
+            Console.WriteLine(" 1) Add Blog");
+            Console.WriteLine(" 0) Go Back");
+
+            Console.Write("> ");
+            string choice = Console.ReadLine();
+            switch (choice)
+            {
+                case "1":
+                    AddBlog();
+                    return this;
+                case "0":
+                    return _parentUI;
+                default:
+                    Console.WriteLine("Invalid Selection");
+                    return this;
+            }
+        }
+
+        private void AddBlog()
+        {
+            Console.WriteLine("New Blog");
+            Blog blog = new Blog();
+
+            Console.Write("Title: ");
+            blog.Title = Console.ReadLine();
+
+            Console.Write("URL: ");
+            blog.Url = Console.ReadLine();
+
+            _blogRepository.Insert(blog);
+        }
+
+        private void ListBlogs()
+        {
+            throw new NotImplementedException();
+        }
+
+        private Blog ChooseBlog(string prompt = null)
+        {
+            throw new NotImplementedException();
+        }
+
+        private void EditBlog()
+        {
+            throw new NotImplementedException();
+        }
+
+        private void RemoveBlog()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
